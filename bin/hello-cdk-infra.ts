0467c54478bd0401ed241f99807eb2f9ecde03e0
Jaypee <jignacio@cambridge.org>
2021-05-10 17:09:10 +0800

    Inilize cdk repository

--- bin/hello-cdk-infra.ts
+++ bin/hello-cdk-infra.ts
@@ -4,18 +4,16 @@ import * as cdk from 'aws-cdk-lib';
 import { HelloCdkInfraStack } from '../lib/hello-cdk-infra-stack';
 
 const app = new cdk.App();
-new HelloCdkInfraStack(app, 'HelloCdkInfraStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+const region : string = app.node.tryGetContext('region') || 'ap-northeast-2';
+const config = {
+  github: {
+      owner: 'jaypeeig',
+      repository: 'hello-cdk',
+  },
+  env: {
+      region: region
+  }
+}
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+new HelloCdkInfraStack(app, 'HelloCdkInfraStack', config);
+app.synth();
--- lib/hello-cdk-infra-stack.ts
+++ lib/hello-cdk-infra-stack.ts
@@ -1,10 +1,100 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { 
+  Stack, 
+  StackProps,
+  RemovalPolicy,
+  CfnOutput,
+  SecretValue,
+  aws_codebuild as codebuild,
+  aws_codepipeline as codepipeline,
+  aws_ssm as ssm,
+  aws_codepipeline_actions as codepipeline_actions, 
+  aws_s3 as s3 
+} from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-
+export interface PipelineProps extends StackProps {
+  github: {
+    owner: string
+    repository: string
+  }
+}
 export class HelloCdkInfraStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: Construct, id: string, props?: PipelineProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucketWebsite = new s3.Bucket(this, 'websiteBucket', {
+      websiteIndexDocument: 'index.html',
+      websiteErrorDocument: 'index.html',
+      publicReadAccess: true,
+      removalPolicy: RemovalPolicy.DESTROY,
+      cors: [
+        {
+           allowedOrigins: ['*'],
+           allowedMethods: [s3.HttpMethods.GET],
+        }
+      ]
+    });
+
+    const outputSources = new codepipeline.Artifact();
+    const outputBuilds = new codepipeline.Artifact();
+
+    const githubToken = ssm.StringParameter.fromStringParameterAttributes(this, 'AccessToken', {
+      parameterName: '/Demo/Github/AccessToken'
+    }).stringValue;
+
+    const buildProject = new codebuild.PipelineProject(this, 'Build Nuxt SPA', {
+      projectName: `HelloCDK-Build`,
+      buildSpec: codebuild.BuildSpec.fromSourceFilename('./codebuild/buildspec.yml'),
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.STANDARD_4_0,
+      },
+    });
+
+    const pipeline = new codepipeline.Pipeline(this, 'Pipeline', {
+      pipelineName: 'HelloCDK',
+      restartExecutionOnUpdate: true
+    });
+
+    pipeline.addStage({
+      stageName: 'Source',
+      actions: [
+        new codepipeline_actions.GitHubSourceAction({
+          actionName: 'Merged',
+          owner: props?.github.owner || 'owner',
+          repo: props?.github.repository || 'hello-cdk',
+          oauthToken: SecretValue.plainText(githubToken),
+          branch: 'develop',
+          output: outputSources,
+          trigger: codepipeline_actions.GitHubTrigger.WEBHOOK
+        })
+      ]
+    });
+
+    pipeline.addStage({
+      stageName: 'Build',
+      actions: [
+        new codepipeline_actions.CodeBuildAction({
+          actionName: 'Build',
+          project: buildProject,
+          input: outputSources,
+          outputs: [outputBuilds]
+        })
+      ]
+    });
+
+    pipeline.addStage({
+      stageName: 'Deploy',
+      actions: [
+        new codepipeline_actions.S3DeployAction({
+          actionName: 'Website',
+          input: outputBuilds,
+          bucket: bucketWebsite
+        })
+      ]
+    });
+
+    new CfnOutput(this, 'WebsiteURL', {
+      value: bucketWebsite.bucketWebsiteUrl,
+      description: 'Website URL',
+    });
   }
 }
